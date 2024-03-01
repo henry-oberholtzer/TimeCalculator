7cb6c65f9f51bd84b6d6de9a6fcb6a82f38cc77b
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-03-01 12:17:31 -0800

    Revert "passes first test"
    This reverts commit 019ec9893cf48faa19a9d081e4fce9d9a6a3de21.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This entrypoint file to be used in development. Start by reading README.md
 from time_calculator import add_time
 from unittest import main
 
--- time_calculator.py
+++ time_calculator.py
@@ -1,11 +1,7 @@
-import re
+def add_time(start, duration):
 
 
-def add_time(start, duration, day=None):
-    nums = re.compile(r'\d{1,}')
-    start_hours = int((start_list := nums.findall(start))[0])
-    start_mins = int(start_list[1])
-    start_am_pm = re.search(r'AM|PM', start)[0]
-    duration_hours = int((duration_list := nums.findall(duration))[0])
-    duration_mins = int(duration_list[1])
-    return f'{start_hours + duration_hours}:{start_mins + duration_mins} {start_am_pm}'
+
+
+
+    return new_time
