c3af5d5c4ca414472218056a424f3de82dc79f9f
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-03-01 12:19:04 -0800

    Revert "Revert "passes first test""
    This reverts commit 7cb6c65f9f51bd84b6d6de9a6fcb6a82f38cc77b.

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,11 @@
-def add_time(start, duration):
+import re
 
 
-
-
-
-    return new_time
+def add_time(start, duration, day=None):
+    nums = re.compile(r'\d{1,}')
+    start_hours = int((start_list := nums.findall(start))[0])
+    start_mins = int(start_list[1])
+    start_am_pm = re.search(r'AM|PM', start)[0]
+    duration_hours = int((duration_list := nums.findall(duration))[0])
+    duration_mins = int(duration_list[1])
+    return f'{start_hours + duration_hours}:{start_mins + duration_mins} {start_am_pm}'
