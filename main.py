08c6b29cc5d76b02238480b9dcb6535646b04785
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-03-01 13:05:30 -0800

    passes second test, refactoring tests to pytest

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from time_calculator import add_time
-from unittest import main
+from pytest import main
 
 
 print(add_time("11:06 PM", "2:02"))
 
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+main(['-vv'])
--- time_calculator.py
+++ time_calculator.py
@@ -5,7 +5,19 @@ def add_time(start, duration, day=None):
     nums = re.compile(r'\d{1,}')
     start_hours = int((start_list := nums.findall(start))[0])
     start_mins = int(start_list[1])
-    start_am_pm = re.search(r'AM|PM', start)[0]
+    am_pm = re.search(r'AM|PM', start)[0]
     duration_hours = int((duration_list := nums.findall(duration))[0])
     duration_mins = int(duration_list[1])
-    return f'{start_hours + duration_hours}:{start_mins + duration_mins} {start_am_pm}'
+
+    sum_mins = start_mins + duration_mins
+    
+    sum_hours = start_hours + duration_hours + round(60 / sum_mins % 1)
+
+    if sum_hours / 12 > 1:
+        if am_pm == 'AM':
+            am_pm = 'PM'
+        else:
+            am_pm = 'AM'
+
+
+    return f'{sum_hours - 12 * round(sum_hours / 12)}:{str(sum_mins % 60).zfill(2)} {am_pm}'
