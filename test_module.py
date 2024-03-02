5378931b3283747e109dfd658e28ffcd3a563adc
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-03-01 16:37:30 -0800

    passes all tests up to day param

--- test_module.py
+++ test_module.py
@@ -19,41 +19,40 @@ test_cases = [
         "9:15 PM", "5:30",
         "2:45 AM (next day)",
         'Expected time to end with "(next day)" when it is the next day.',
-        id="text_next_day"
+        id="test_next_day"
     ),
     pytest.param(
         "11:40 AM", "0:25",
         "12:05 PM",
         'Expected period to change from AM to PM at 12:00',
-        id="text_period_change_at_twelve"
+        id="test_period_change_at_twelve"
     ),
     pytest.param(
         "2:59 AM", "24:00",
         "2:59 AM (next day)",
         'Expected calling "add_time()" with "2:59 AM", "24:00" to return "2:59 AM"',
-        id="text_twenty_four"
-    )
+        id="test_twenty_four"
+    ),
+    pytest.param(
+        "11:59 PM", "24:05",
+        "12:04 AM (2 days later)",
+        'Expected calling "add_time()" with "11:59 PM", "24:05" to return "12:04 AM (2 days later)"',
+        id="test_two_days_later"
+    ),
+    pytest.param(
+        "8:16 PM", "466:02",
+        "6:18 AM (20 days later)",
+        'Expected calling "add_time()" with "8:16 PM", "466:02" to return "6:18 AM (20 days later)"',
+        id="test_high_duration"
+    ),
+    pytest.param(
+        "5:01 AM", "0:00",
+        "5:01 AM",
+        'Expected adding 0:00 to return initial time.',
+        id="test_no_change"
+    ),
+
 ]
-
-#     def test_twenty_four(self):
-#         actual = add_time()
-#         expected = 
-#         self.assertEqual(actual, expected, )
-
-#     def test_two_days_later(self):
-#         actual = add_time("11:59 PM", "24:05")
-#         expected = "12:04 AM (2 days later)"
-#         self.assertEqual(actual, expected, 'Expected calling "add_time()" with "11:59 PM", "24:05" to return "12:04 AM (2 days later)"')
-
-#     def test_high_duration(self):
-#         actual = add_time("8:16 PM", "466:02")
-#         expected = "6:18 AM (20 days later)"
-#         self.assertEqual(actual, expected, 'Expected calling "add_time()" with "8:16 PM", "466:02" to return "6:18 AM (20 days later)"')
-
-#     def test_no_change(self):
-#         actual = add_time("5:01 AM", "0:00")
-#         expected = "5:01 AM"
-#         self.assertEqual(actual, expected, 'Expected adding 0:00 to return initial time.')
 
 #     def test_same_period_with_day(self):
 #         actual = add_time("3:30 PM", "2:12", "Monday")
@@ -74,9 +73,6 @@ test_cases = [
 #         actual = add_time("8:16 PM", "466:02", "tuesday")
 #         expected = "6:18 AM, Monday (20 days later)"
 #         self.assertEqual(actual, expected, 'Expected calling "add_time()" with "8:16 PM", "466:02", "tuesday" to return "6:18 AM, Monday (20 days later)"')
-
-# if __name__ == "__main__":
-#     unittest.main()
 
 @pytest.mark.parametrize('start,duration,expected_output,fail_message', test_cases)
 def test_template(start, duration, expected_output, fail_message):
